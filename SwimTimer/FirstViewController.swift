a05394290358611d4af0bd44b70ebeb206317ea4
Viktor Fägerlind <viktor_fagerlind@hotmail.com>
2015-05-22 21:34:37 +0200

    Initial Commit

---
+++ SwimTimer/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SwimTimer
+//
+//  Created by Viktor Fägerlind on 22/05/15.
+//  Copyright (c) 2015 Viktor Fägerlind. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
