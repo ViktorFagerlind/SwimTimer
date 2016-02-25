ab9ad17d7cd25186020ee37628707fbd783b5ea1
Viktor Fägerlind <viktor_fagerlind@hotmail.com>
2016-02-25 12:28:41 +0100

    Added new files

---
+++ SwimTimer/SwimmerResultCellController.swift
@@ -0,0 +1,15 @@
+//
+//  SwimsTableViewCell.swift
+//  SwimTimer
+//
+//  Created by Viktor Fägerlind on 23/02/16.
+//  Copyright © 2016 Viktor Fägerlind. All rights reserved.
+//
+
+import UIKit
+
+class SwimmerResultCellController: UITableViewCell
+{
+  @IBOutlet var nameLabel: UILabel!
+  @IBOutlet var timeLabel: UILabel!
+}
